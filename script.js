277372c834bfe0a937523e8ced661114782325c1
Luiz G. <guga.santos0810@gmail.com>
2023-09-05 14:48:19 -0300

    udpate

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function toggleMode() {
   html = document.documentElement
   html.classList.toggle("light")
+
+  img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/pt-light.jpg")
+    img.setAttribute(
+      "alt",
+      "Foto de Luiz sorrindo usando camisa jeans, barba e fundo preto"
+    )
+  } else {
+    img.setAttribute("src", "./assets/pt-dark.jpg")
+    img.setAttribute(
+      "alt",
+      "Foto de Luiz sorrindo usando camisa jeans, barba e fundo branco"
+    )
+  }
 }
